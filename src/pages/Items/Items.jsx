20e907614010c98de210d6255f11265167d93646
HwanMin <65848374+JEONGHWANMIN@users.noreply.github.com>
2024-04-13 20:57:09 +0900

    Feat: api

--- src/pages/Items/Items.jsx
+++ src/pages/Items/Items.jsx
@@ -1,4 +1,7 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { styled } from "styled-components";
+import { axiosInstance } from "~/apis/axiosInstance";
 import Header from "~/components/Items/Header/Header";
 import BottomSection from "~/components/Items/Sections/BottomSection/BottomSection";
 import Pagenation from "~/components/Items/Sections/Footer/Pagenation";
@@ -6,6 +9,17 @@ import TopSection from "~/components/Items/Sections/TopSection/TopSection";
 import Swagger from "~/service/productApi.jsx";
 
 function Items() {
+  const [productList, setProductList] = useState([]);
+  const [count, setTotalCount] = useState(0);
+  useEffect(() => {
+    axiosInstance
+      .get("/products")
+      .then((response) => response.data)
+      .then((data) => {
+        setProductList(data?.list);
+        setTotalCount(data?.totalCount);
+      });
+  }, []);
   return (
     <>
       <Header />
